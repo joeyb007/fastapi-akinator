902cb8db338a8dfd06dcee3bb0fa04b2e6b5f0c1
Joseph Charles Dasilva Barbosa <josephbarbosa416@gmail.com>
2025-11-25 13:39:12 -0500

    fixed api

--- api.py
+++ api.py
@@ -2,22 +2,22 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import requests
 from fastapi.middleware.cors import CORSMiddleware
+from groq import Groq
+from dotenv import load_dotenv
 import os
-
-
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React dev server
+    allow_origins=["*"], 
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-HF_API_URL = "https://api-inference.huggingface.co/models/google/flan-t5-small"
-HF_API_KEY = os.getenv("HF_API_KEY")
-
+load_dotenv()
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
 
 class Question(BaseModel):
     question: str 
@@ -36,24 +36,13 @@ def guess_answer(g: Guess):
 @app.post("/ask")
 def ask_question(q: Question):
     prompt = f"Answer ONLY yes or no to this question about a radio: {q.question}"
+    chat_completion = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {"role": "user", "content": prompt}
+        ],
+    )
 
-    headers = {
-        "Authorization": f"Bearer {HF_API_KEY}",
-        "Content-Type": "application/json"
-    }
-
-    payload = {
-        "inputs": prompt,
-        "parameters": {"max_new_tokens": 50}
-    }
-
-    response = requests.post(HF_API_URL, headers=headers, json=payload)
-    
-    if response.status_code != 200:
-        return {"response": "Error contacting model API"}
-
-    model_out = response.json()
-    generated_text = model_out[0]["generated_text"]
-
+    generated_text = chat_completion.choices[0].message.content
     answer = "Yes" if "yes" in generated_text.lower() else "No"
     return {"response": answer}
